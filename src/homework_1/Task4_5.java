32f582868da3f7c89a2c4c625350e8577010eba6
Galya Livyant <livyantgalina@gmail.com>
2023-09-21 19:21:27 +0300

    Исправила файл Task4_5

--- src/homework_1/Task4_5.java
+++ src/homework_1/Task4_5.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 public class Task4_5 {
     public static void main(String[] args) {
-        char ch = 'й'; //создание переменное и ее инициализация
+        Scanner in = new Scanner(System.in);
+        //ввод пользователем символа
+        System.out.print("Введите символ: ");
+        char ch = in.nextLine().charAt(0);
         if (Character.isLetter(ch)) { //проверка переменной а
             System.out.println("Символ " + ch + " является буквой"); //печать результата
         }
