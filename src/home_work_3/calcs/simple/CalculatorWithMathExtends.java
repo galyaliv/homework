5248a6517c31485826081a92d7c324763c9294d6
Galya Livyant <livyantgalina@gmail.com>
2023-10-17 19:11:04 +0300

    Исправила ошибку в коде

--- src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
+++ src/home_work_3/calcs/simple/CalculatorWithMathExtends.java
@@ -3,16 +3,13 @@ package home_work_3.calcs.simple;
 import home_work_3.calcs.api.ICalculator;
 
 public class CalculatorWithMathExtends extends CalculatorWithOperator implements ICalculator {
-    public double subtract(double minuend, double subtrahend){
-        return minuend - subtrahend;
-    }
-
     /**
      * Метод возводит дробное положительное число в целую степень
      * @param indicator основание степени
      * @param degree показатель степени
      * @return результат возведения основания в степень. В случае некорректного основания возвращает -1
      */
+    @Override
     public double pow(double indicator, int degree){
         if(indicator <= 0){
             return -1;
@@ -25,6 +22,7 @@ public class CalculatorWithMathExtends extends CalculatorWithOperator implements
      * @param number число, для которого определяется модуль
      * @return модуль числа типа double
      */
+    @Override
     public double absolute(double number){
         return Math.abs(number);
     }
@@ -34,6 +32,7 @@ public class CalculatorWithMathExtends extends CalculatorWithOperator implements
      * @param number число, для которого определяется модуль
      * @return модуль числа типа int
      */
+    @Override
     public int absolute(int number){
         return Math.abs(number);
     }
@@ -43,6 +42,7 @@ public class CalculatorWithMathExtends extends CalculatorWithOperator implements
      * @param number число, из которого извлекается корень
      * @return квадратный корень числа. В случае некорректного ввода числа, возвращает -1
      */
+    @Override
     public double root(int number){
         if (number < 0){
             return -1;
